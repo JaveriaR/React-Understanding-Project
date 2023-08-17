5e5f73acf783b5fee86c13328c191c7f2e6b91bb
JaveriaR <javeria.rehman@rsystems.com>
2023-08-17 16:16:26 +0530

    Core react code

---
+++ App.js
@@ -0,0 +1,14 @@
+const Heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Hello H1!"),
+    React.createElement("h2", {}, "Hello H2!"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Hello H1!"),
+    React.createElement("h2", {}, "Hello H2!"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(Heading);
